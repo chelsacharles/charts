873c58812d6704d98fdafe1aac77894dba209f15
Chelsa <charleschelsa@gmail.com>
2020-07-02 22:14:28 +0530

    ..

--- lib/lineChart.dart
+++ lib/lineChart.dart
@@ -23,34 +23,34 @@ class HorizontalBarChart3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final customTickFormatter =
-charts.BasicNumericTickFormatterSpec((num value) {
-  if (value == 1) {
-    return "Jan";
-  } else if (value == 2) {
-    return "Feb";
-  } else if (value == 3) {
-    return "Mar";
-  } else if (value == 4) {
-    return "Apr";
-  } else if (value == 5) {
-    return "May";
-  } else if (value == 6) {
-    return "Jun";
-  } else if (value == 7) {
-    return "Jul";
-  }else if (value == 8) {
-    return "Aug";
-  }else if (value == 9) {
-    return "Sep";
-  }else if (value == 10) {
-    return "Oct";
-  }else if (value == 11) {
-    return "Nov";
-  }else if (value == 12) {
-    return "Dec";
-  }
-});
+//     final customTickFormatter =
+// charts.BasicNumericTickFormatterSpec((num value) {
+//   if (value == 1) {
+//     return "Jan";
+//   } else if (value == 2) {
+//     return "Feb";
+//   } else if (value == 3) {
+//     return "Mar";
+//   } else if (value == 4) {
+//     return "Apr";
+//   } else if (value == 5) {
+//     return "May";
+//   } else if (value == 6) {
+//     return "Jun";
+//   } else if (value == 7) {
+//     return "Jul";
+//   }else if (value == 8) {
+//     return "Aug";
+//   }else if (value == 9) {
+//     return "Sep";
+//   }else if (value == 10) {
+//     return "Oct";
+//   }else if (value == 11) {
+//     return "Nov";
+//   }else if (value == 12) {
+//     return "Dec";
+//   }
+// });
 
     // For horizontal bar charts, set the [vertical] flag to false.
     return new charts.LineChart(
@@ -62,12 +62,12 @@ charts.BasicNumericTickFormatterSpec((num value) {
                              primaryMeasureAxis: new charts.NumericAxisSpec(
                                
          renderSpec: new charts.NoneRenderSpec()),
-                  domainAxis: charts.NumericAxisSpec(
+    //               domainAxis: charts.NumericAxisSpec(
                    
-      tickProviderSpec:
-      charts.BasicNumericTickProviderSpec(desiredTickCount: 12),
-      tickFormatterSpec: customTickFormatter,
-    ),
+    //   tickProviderSpec:
+    //   charts.BasicNumericTickProviderSpec(desiredTickCount: 12),
+    //   tickFormatterSpec: customTickFormatter,
+    // ),
       
     );
   }
