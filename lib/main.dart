2ca211f5dc1ed956b07db786f7a8b0806c627df8
Chelsa <charleschelsa@gmail.com>
2020-07-02 18:13:54 +0530

    Size issue rectififed

--- lib/main.dart
+++ lib/main.dart
@@ -29,17 +29,52 @@ class MyApp extends StatelessWidget {
        child: Stack(children: <Widget>[
          Align(
            alignment: Alignment.centerRight,
-           child: Stack(
+           child: Column(
              children: <Widget>[
-               Padding(
-               padding: EdgeInsets.fromLTRB(20, 20, 20, 350),
-               child:  Charts1(),),
-              Padding(
-               padding: EdgeInsets.fromLTRB(20, 200, 20, 180),
-               child:  Charts3(),),
-               Padding(
-               padding: EdgeInsets.fromLTRB(20, 350, 20, 20),
-               child:  Charts(),),
+              //  Padding(
+              //  padding: EdgeInsets.fromLTRB(20, 20, 20, 350),
+              //  child:  Charts1(),),
+              // Padding(
+              //  padding: EdgeInsets.fromLTRB(20, 200, 20, 180),
+              //  child:  Charts3(),),
+              //  Padding(
+              //  padding: EdgeInsets.fromLTRB(20, 350, 20, 20),
+              //  child:  Charts(),),
+               Expanded(
+          child: Card(
+              elevation: 0,
+              margin: EdgeInsets.all(5),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Charts1(),
+              )),
+        ),
+
+      Expanded(
+          child: Card(
+              elevation: 0,
+              margin: EdgeInsets.all(5),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Charts3(),
+                ),
+              )),
+        ),
+             Expanded(
+          child: Card(
+              elevation: 0,
+              margin: EdgeInsets.all(5),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Charts(),
+                ),
+              )),
+        ),
+              
                ]))])))
       // Container(
       //   padding: EdgeInsets.fromLTRB(20, 20, 20, 350),
